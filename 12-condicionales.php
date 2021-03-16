92e10a02cd08543ddaa65556ab3a3ab24e36f11e
EduardoCollazoR <eduardo.collazo16@tectijuana.edu.mx>
2021-03-15 21:27:36 -0700

    agregando

--- 12-condicionales.php
+++ 12-condicionales.php
@@ -1,7 +1,59 @@
 <?php include 'includes/header.php';
 
+$autenticado = true;
+$admin = true;
+if ($autenticado && $admin) {
+    echo 'Usuario autenticado';
+} else {
+    echo 'Usuario no autenticado, inicia sesion';
+}
+echo "<br>";
+
+// if anidado
+$cliente = [
+    'nombre' => 'Juan',
+    'saldo' => 200,
+    'informacion' => [
+        'tipo' => 'Premium'
+    ]
+];
+
+if (!empty($cliente)) {
+    echo 'el arreglo no esta vacio';
+    if ($cliente['saldo'] > 0) {
+        echo 'el saldo del cliente esta disponible';
+    } else {
+        echo 'no hay saldo';
+    }
+}
+
+echo "<br>";
+
+// else if 
+if ($cliente['saldo'] > 0) {
+    echo 'El cliente tiene saldo';
+} else if ($cliente['informacion']['tipo'] === 'Premium') {
+    echo 'el cliente es premium';
+} else {
+    echo 'no hay cliente definido o no tiene saldo o no es premium';
+}
 
 
-
-
+//switch 
+echo "<br>";
+$tecnologia = "PHP";
+switch ($tecnologia) {
+    case 'PHP':
+        echo "PHP";
+        break;
+    case 'JavaScript';
+        echo 'Genial';
+        break;
+    case 'HTML';
+        echo 'emm..';
+        break;
+    default:
+        echo "Algun otro lenguaje";
+        break;
+}
 include 'includes/footer.php';
--- 13-loops.php
+++ 13-loops.php
@@ -1,6 +1,41 @@
 <?php include 'includes/header.php';
 
+// while 
+$i = 0; // inicializador
+
+while ($i < 10) {
+    echo $i . "<br>";
+    $i++; // incremento
+}
+
+// do while
+echo "<br>";
+$i = 0;
+do {
+    echo $i . "<br>";
+    $i++;
+} while ($i < 10);
+
+echo "<br>";
+
+/** 
+ * 3 imprimir fizz
+ * 5 imprimir buzz
+ * 3 y 5 imprimir fizz buzz
+ */
 
 
+// for loop
+// for ($i = 1; $i < 100; $i++) {
+//     if ($i % 3 === 0 && $i % 5 === 0) {
+//         echo $i . " - fizzbuzz <br/>";
+//     } else if ($i % 3 === 0) {
+//         echo $i . " - fizz <br/>";
+//     } else if ($i % 5 === 0) {
+//         echo $i . " - buzz <br/>";
+//     } else {
+//         echo $i . "<br/>";
+//     }
+// }
 
 include 'includes/footer.php';
